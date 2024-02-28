160464d444e4a24595a3b7e00c35b144f9cec490
Tiago Souza <tirigopeixe@gmail.com>
2024-02-28 17:23:56 -0300

    Introdução

---
+++ 01-intro-python/variaveis-constantes.py
@@ -0,0 +1,48 @@
+# identificadores
+# letras, numeros
+# não pode ser palavras reservadas
+# if, for, class...
+# case sensitive
+a = 10
+A = 10
+nome = "silvio"
+Nome = "silvio"
+
+# variaveis
+# identificador = literal (valor)
+# snake_case (convenção)
+idade = 10 #int
+nome = "kaio" #str
+pessoa_juridica = "Google" #str
+imposto_renda = 2000.00 #float
+verdade = True #bool
+
+# Java 
+# Tipo identificador = valor
+# String nome = "Maria"
+# Tipagem Estática
+
+# Tipo existe no python
+# Inferência de tipo
+# Tipagem Dinâmica
+
+# Constantes
+# Valor não muda após definido
+# Não existe constante em python
+# Tudo Maiúsculo
+PI = 3.14
+
+# Comentarios Unica Linha
+"""
+multiplas linhas
+"""
+
+# docstring
+# comentario de documentação
+def somar(n1, n2):
+    '''
+    Função que soma dois números
+    :param n1: primeiro número
+    :param n2: segundo número
+    '''
+    return n1 + n2
